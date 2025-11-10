82c1a75395389bb5752b04ed533b4d70418d64aa
KikyAja31 <2411500032@mahasiswa.atmaluhur.ac.id>
2025-11-11 00:47:46 +0700

    edit: sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,6 +7,7 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
                 </a>
+
                 <div class="sb-sidenav-menu-heading">Manajemen</div>
                 <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                     <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
@@ -19,15 +20,16 @@
                         <a class="nav-link" href="layout-sidenav-light.html">Tambah Buku</a>
                     </nav>
                 </div>
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+
+                <a class="nav-link <?php echo ($page == "daftar-kategori" || $page == "tambah-kategori" || $page == "edit-kategori") ? "active" : "collapsed" ?>" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
                     <div class="sb-nav-link-icon"><i class="fas fa-folder-open"></i></div>
                     Kategori
                     <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                 </a>
-                <div class="collapse" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                <div class="collapse <?php echo ($page == "daftar-kategori" || $page == "tambah-kategori" || $page == "edit-kategori") ? "show" : "" ?>" id="collapsePages" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
                     <nav class="sb-sidenav-menu-nested nav">
-                        <a class="nav-link" href="layout-static.html">Daftar Kategori</a>
-                        <a class="nav-link" href="layout-sidenav-light.html">Tambah Kategori</a>
+                        <a class="nav-link <?php echo ($page == "daftar-kategori")? 'active' : '';  ?>"  href="index.php?hal=daftar-kategori">Daftar Kategori</a>
+                        <a class="nav-link <?php echo ($page == "tambah-kategori")? 'active' : '';  ?>" href="index.php?hal=tambah-kategori">Tambah Kategori</a>
                     </nav>
                 </div>
 
